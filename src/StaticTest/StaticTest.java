a71fc7fc5d978619e24e2377338b0988bbebd7b0
zhaoshukai <pheonix0862@hotmail.com>
2020-04-06 21:00:13 +0800

    静态方法的使用

---
+++ src/StaticTest/StaticTest.java
@@ -0,0 +1,46 @@
+package StaticTest;
+
+public class StaticTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Employee{
+	private static int nextId = 1;
+	
+	private String name;
+	private double salary;
+	private int id;
+	
+	public Employee(String n, double s) {
+		name = n;
+		salary = s;
+		id = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId() {
+		id = nextId;
+		nextId++;
+	}
+	
+	public static void main(String[] args) {
+		Employee e = new Employee("Tom", 50000);
+		System.out.println(e.getName()+" "+e.getSalary());
+	}
+}
